7db3da2fd107590185f7b9425b57647bef1fdf82
System Administrator <root@Thomass-MacBook-Pro.local>
2015-01-02 16:05:20 -0500

    Added example shell script

---
+++ example_cmd.sh
@@ -0,0 +1,6 @@
+bams=`ls /Users/tfwillems/Desktop/Club_files/chry/final_callset/chrY_BAMs/filtered_bams/*.chrY.bam -m | tr -d '\n'`
+bais=`ls /Users/tfwillems/Desktop/Club_files/chry/final_callset/chrY_BAMs/filtered_bams/*.chrY.bam.bai -m | tr -d '\n'`
+fasta_dir="/Users/tfwillems/Desktop/Coding/dbase/"
+vizalign="/Users/tfwillems/Desktop/Coding/HipSTR/vizalign"
+
+./web_server.py --bams $bams --bais $bais --fasta $fasta_dir --vizalign $vizalign
